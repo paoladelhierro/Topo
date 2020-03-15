1de3e112b077a5fe6c40a23fe28fdd9411786437
ekiMGlz <mike.glz.96@gmail.com>
2020-03-15 16:44:28 -0600

    ???

---
+++ src/interfaces/WAMRoom.java
@@ -0,0 +1,19 @@
+package interfaces;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import server.Player;
+
+/**
+ * WAMRoom
+ */
+public interface WAMRoom extends Remote{
+
+    public void addUser(String user) throws RemoteException;
+    public int addPoint(String user) throws RemoteException;
+    public ArrayList<Player> getScore() throws RemoteException;
+    public boolean done() throws RemoteException;
+    public void reset() throws RemoteException;
+
+}
---
+++ src/server/GameThread.java
@@ -0,0 +1,17 @@
+package server;
+
+/**
+ * GameThread
+ */
+public class GameThread implements Runnable{
+
+    public GameThread(String hostPort, String multiIP){
+        
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+}
