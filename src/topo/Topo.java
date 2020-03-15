aa78885c386719cab5de5516f8d520cbb3b6bd29
Paola Del Hierro <p.del.hierro@hotmail.com>
2020-03-15 17:04:56 -0600

    juego front

--- src/topo/Topo.java
+++ src/topo/Topo.java
@@ -5,17 +5,49 @@
  */
 package topo;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author hca
  */
-public class Topo {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Topo extends Thread{
+    private javax.swing.JRadioButton[] radiobuttons = new javax.swing.JRadioButton[9];
+    private boolean keepGoing = true;
+    int mole;
+    
+    public Topo(javax.swing.JRadioButton[] radiobuttons){
+        this.radiobuttons = radiobuttons;
+        
     }
+    private int changeMole(int mole){
+        int m = (int)(Math.random()*9);
+        radiobuttons[mole].setText((mole+1) + ".");
+        radiobuttons[m].setText("(u.u)");
+        return m;
+    }
+
+    @Override
+    public void run(){
+        mole = (int)(Math.random()*9);
+        while(keepGoing){
+            mole = changeMole(mole);
+            try {
+                this.sleep(5000);
+                if(radiobuttons[mole].isSelected()){
+                    System.out.println("Gano");
+                    radiobuttons[mole].setSelected(false);
+                    radiobuttons[mole].setText((mole+1)+".");
+                    keepGoing=  false;   
+                    break;
+                }
+                
+            } catch (InterruptedException ex) {
+            Logger.getLogger(Topo.class.getName()).log(Level.SEVERE, null, ex);
+            }   
+        } 
+    }
+ 
     
 }
